3370bd0aa1326ffa301b1e9d7cd77fb87918e597
teamamisimedishvili <127042618+teamamisimedishvili@users.noreply.github.com>
2024-02-21 11:35:17 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+class Cube {
+    height;
+    width;
+    length;
+    
+    cube_cube () {
+        console.log (this.height);
+        console.log (this.width);
+        console.log (this.length);
+    
+    }
+}
+    var obieqti = new Cube() 
+    obieqti.width = 300
+    obieqti.height = 105
+    obieqti.length = 205
+
+    obieqti.cube_cube()
